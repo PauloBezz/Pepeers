04bad931bdfee3cbab05020b82187344aa052e28
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-07 23:58:46 -0300

    feat: create Main component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,10 @@
-import Header from '../../components/Header'
+import Main from '../../components/Main'
 import './styles.css'
 
 export function Home() {
     return (
         <>
-            <Header />
+            <Main/>
         </>
     )
 }
