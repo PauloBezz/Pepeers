a8d8abab3aac550ebce4907293baec92b2a14c5d
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-08 01:47:51 -0300

    refactor: Main component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,30 +1,32 @@
-import Header from '../Header'
+import outono from './assets/Outono.svg'
 import casual from './assets/Casual.svg'
 import moleton from './assets/Moleton.svg'
 import primavera from './assets/Primavera.svg'
-import outono from './assets/Outono.svg'
+import Arrow from './assets/Arrow'
 import './styles.css'
 
 export default function Main() {
     return (
-        <>
-            <Header />
-            <section>
-                <span className='title'>
-                    <h1>Seu poder,
-                        <h1>Nosso conjunto!</h1>
-                    </h1>
-                </span>
+        <section className='main-container'>
+            <span className='main-title'>
+                <h1>Seu poder,</h1>
+                <h1>Nosso conjunto!</h1>
+            </span>
 
-                <div>
-                    <img src={outono} alt="" />
-                    <img src={casual} alt="" />
-                    <img src={moleton} alt="" />
-                    <img src={primavera} alt="" />
-                </div>
+            <div className='main-carrossel'>
+                <img src={outono} alt="Modelo Outono" title='Outono' />
+                <img src={casual} alt="Modelo Casual" title='Casual'/>
+                <img src={moleton} alt="Modelo Moleton" title="Moleton" />
+                <img src={primavera} alt="Modelo Primavera" title="Primavera" />
+            </div>
 
-                <button>Coringa?</button>
-            </section>
-        </>
+            <button className='main-button'>Coringa?<Arrow/></button>
+            <div className='main-notice'>
+                <p>Frete grátis até 12/10/2024</p>
+            </div>
+
+        </section>
     )
 }
+
+/* */
