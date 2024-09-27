b08546aeff9d6c937d4b6246a0be3b313ca2579b
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-26 23:57:59 -0300

    refactor: Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import Header from '../../components/Header'
 import Main from '../../components/Main'
+import Sidebar from '../../components/Sidebar'
 import './styles.css'
 
 export function Home() {
     return (
         <>
             <Header />
+            <Sidebar/>
             <Main />
         </>
     )
