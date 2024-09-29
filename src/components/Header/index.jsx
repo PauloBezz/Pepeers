1fb9462ca580904204fa4ecf0130c16926e12c3a
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-28 22:22:59 -0300

    feat: add class in Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import logo from './assets/img/logo.svg'
 import search from './assets/img/search.svg'
 import cart from './assets/img/cart.svg'
 import menu from './assets/img/menu.svg'
+import config from './assets/img/config.svg'
 import './styles.css'
 
 export default function Header() {
@@ -11,9 +12,10 @@ export default function Header() {
                 <img src={logo} alt="Logo" title='Logo' className='logo' />
             </a>
             <div className='icons'>
-                <img src={search} alt="Search" title='Search' />
+                <img src={search} alt="Search" className='icon-search' title='Search' />
                 <img src={cart} alt="Cart" title='Cart' />
-                <img src={menu} alt="Menu" title='Menu' />
+                <img src={menu} alt="Menu" className='icon-menu' title='Menu' />
+                <img src={config} alt="Options" title='Options' />
             </div>
         </header>
     )
