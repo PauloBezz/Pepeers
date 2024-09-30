fcb229d4bb528a27fe963d4736a92ea360eabd7a
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-29 23:56:47 -0300

    feat: add route for Login page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { Home } from '../pages/Home/index'
+import Login from '../pages/Login'
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route Component={Home} path="/" />
+                <Route Component={Login} path="/login" />
             </Routes>
         </BrowserRouter>
     )
