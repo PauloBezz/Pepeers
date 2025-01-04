2dbda315b3dc6abbf8a9bb171925b6a73dc70972
PauloBezz <paulo.bezerra2057@gmail.com>
2025-01-03 23:47:11 -0300

    refactor: responsive in Home page

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,9 @@
+import './styles.css'
 import outono from './assets/Outono.svg'
 import casual from './assets/Casual.svg'
 import moleton from './assets/Moleton.svg'
 import primavera from './assets/Primavera.svg'
 import Arrow from './assets/Arrow'
-import './styles.css'
 
 export default function Main() {
     return (
@@ -14,31 +14,31 @@ export default function Main() {
                 </h1>
 
                 <div className='main-title'>
-                    <h1 className=''> nosso </h1>
+                    <h1>nosso</h1>
                     <h1 className='green'>conjunto!</h1>
                 </div>
             </span>
 
             <figure className='main-carrossel'>
-                <div>
-                    <img src={outono} alt="Modelo Outono" title='Outono' />
-                    <p className='category'>Outono Pink</p>
-                </div>
-
-                <div>
-
-                    <img src={casual} alt="Modelo Casual" title='Casual' />
-                    <p className='category'>Casual</p>
-                </div>
-
-                <div className='carrossel-top'>
-                    <img src={moleton} alt="Modelo Moleton" title="Moleton" />
-                    <p className='category'>Moleton</p>
-                </div>
-
-                <div className='carrossel-top'>
-                    <img src={primavera} alt="Modelo Primavera" title="Primavera" />
-                    <p className='category'>Primavera</p>
+                <article className='first'>
+                    <div>
+                        <img src={outono} alt="Modelo Outono" title='Outono' />
+                        <p className='category'>Outono Pink</p>
+                    </div>
+                    <div>
+                        <img src={casual} alt="Modelo Casual" title='Casual' />
+                        <p className='category'>Casual</p>
+                    </div>
+                </article>
+                <div className='second'>
+                    <div >
+                        <img src={moleton} alt="Modelo Moleton" title="Moleton" />
+                        <p className='category'>Moleton</p>
+                    </div>
+                    <div >
+                        <img src={primavera} alt="Modelo Primavera" title="Primavera" />
+                        <p className='category'>Primavera</p>
+                    </div>
                 </div>
             </figure>
 
