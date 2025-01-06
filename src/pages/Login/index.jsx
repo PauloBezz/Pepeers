fcb697e4c91bbc4f5929fdd807cc1cbc0281d438
PauloBezz <paulo.bezerra2057@gmail.com>
2025-01-05 22:56:17 -0300

    refactor: Login Page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,36 +4,30 @@ import './styles.css'
 
 export default function Login() {
     return (
-        <>
-                <div className='login-image'>
-                    <img src={backimage} alt="" />
-                </div>
-            <main className='login-container'>
-
+        <main className='login-container'>
+            <section className='login-content'>
                 <a href="/login">
                     <img src={logo} alt="" className='login-logo' />
                 </a>
 
                 <h3>Moda feminina e realização</h3>
 
-                <section>
-                    <h1>Login</h1>
+                <h1>Login</h1>
 
-                    <div>
-                        <article>
-                            <h5>Usuário</h5>
-                            <input type="user" />
-                        </article>
-                        <article>
-                            <h5>Senha</h5>
-                            <input type="name" />
-                        </article>
-                        <p>Esqueceu a senha?</p>
+                <div className='inputs'>
+                    <article>
+                        <h5>Usuário</h5>
+                        <input type="user" />
+                    </article>
+                    <article>
+                        <h5>Senha</h5>
+                        <input type="password" />
+                    </article>
+                    <a>Esqueceu a senha?</a>
 
-                        <button>Continuar</button>
-                    </div>
-                </section>
-            </main>
-        </>
+                </div>
+                    <button className='btn-login'>Continuar</button>
+            </section>
+        </main>
     )
 }
