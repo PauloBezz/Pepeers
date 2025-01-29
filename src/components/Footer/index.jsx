c90d305bcacc6cfbf4ca2ee7ef9ae1c97ae29fd0
PauloBezz <paulo.bezerra2057@gmail.com>
2025-01-28 23:57:35 -0300

    feat: add Footer style

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,14 +6,17 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
     return (
         <footer className='footer-container'>
-            <img src={logo} alt="logo" className='footer-logo' />
-            <img src={logoSmall} alt="logo" className='footer-logoSmall' />
-            
-            <p>Copyright 2023-2025 Pepeeps. Todos direitos reservados ao uso.</p>
+            <div className='footer-content'>
+                <img src={logo} alt="logo" className='footer-logo' />
+                <img src={logoSmall} alt="logo" className='footer-logoSmall' />
 
-            <div>
-                <Link>Contato</Link>
-                <Link>Trabalhe conosco</Link>
+
+                <p>Copyright@ 2023-2025 Pepeeps. Todos direitos reservados ao uso.</p>
+                <div className='footer-links'>
+                    <Link>Contato</Link>
+                    <Link>Trabalhe conosco</Link>
+                </div>
+
             </div>
         </footer>
     )
