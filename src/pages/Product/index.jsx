842fc9f5752c9011de9a728f3c7ef6ab5ff049e5
PauloBezz <paulo.bezerra2057@gmail.com>
2025-01-29 23:59:05 -0300

    feat: add Product page

---
+++ src/pages/Product/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import Selection from "../../components/Selection";
+
+
+
+export default function Product() {
+    return (
+        <>
+            <Header />
+            <Selection />
+        </>
+    )
+}
