c470aa76e4111d381ff0c6c3894f731fee82ceb7
PauloBezz <paulo.bezerra2057@gmail.com>
2025-02-20 23:41:24 -0300

    feat: add Cart component

---
+++ src/components/Cart/index.jsx
@@ -0,0 +1,14 @@
+// import "./styles.css"
+
+export const Cart = () => {
+
+    return (
+        <section>
+            <div>
+                <h1>Meus pedidos</h1>
+                <div></div>
+                <button>Finalizar Compra</button>
+            </div>
+        </section>
+    )
+}
