482242bdb5324e6035d63bd5dbcd12b1e11bf6e2
PauloBezz <paulo.bezerra2057@gmail.com>
2025-02-23 23:57:54 -0300

    feat: add About component

---
+++ src/components/About/index.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function About() {
+    return (
+        <section>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <h1>Nossa História</h1>
+            <p>Desde 2020, estamos criando tendências espetaculares e trabalhando na evolução da marca.</p>
+        </section>
+    )
+}
