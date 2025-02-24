86482c50195328cfe321e0b1022a7d563c8067ce
PauloBezz <paulo.bezerra2057@gmail.com>
2025-02-24 01:19:08 -0300

    feat: add About component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,12 +3,14 @@
 
 export default function About() {
     return (
-        <section>
-            <div>
+        <section className="container-about">
+            <div className="about-image">
                 <img src="" alt="" />
             </div>
-            <h1>Nossa História</h1>
-            <p>Desde 2020, estamos criando tendências espetaculares e trabalhando na evolução da marca.</p>
+            <span>
+                <h1>Nossa História</h1>
+                <p>Desde 2020, estamos criando tendências espetaculares e trabalhando na evolução da marca.</p>
+            </span>
         </section>
     )
 }
