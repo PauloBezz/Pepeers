1aecfc8d90af742271543d47a22abe16539775fc
PauloBezz <paulo.bezerra2057@gmail.com>
2025-02-24 01:18:49 -0300

    feat: add Contact page

---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,14 @@
+import About from "../../components/About"
+import Header from "../../components/Header"
+
+
+export const Contact = () => {
+
+    
+    return (
+        <>
+            <Header/>
+            <About/>
+        </>
+    )
+}
