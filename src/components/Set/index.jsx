d94fa1bff679382e8c6e65f715bb0fff8b723409
PauloBezz <paulo.bezerra2057@gmail.com>
2025-02-27 23:58:44 -0300

    feat: add sector component

---
+++ src/components/Set/index.jsx
@@ -0,0 +1,34 @@
+
+
+
+
+
+export default function AllSector() {
+
+
+    return (
+        <main>
+            <div>
+
+            </div>
+        </main>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+// export default function Set() {
+//     return (
+//         <>
+
+//         </>
+//     )
+// }
