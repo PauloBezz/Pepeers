13303dacb30c6cc5bb139e2e6f5311033c6c729f
PauloBezz <paulo.bezerra2057@gmail.com>
2025-03-01 23:57:21 -0300

    feat: add dinamic route in Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,15 +1,18 @@
 import './styles.css'
 import { Search } from '../Search'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
-export default function Sidebar() {
+export default function Sidebar({ data = [] }) {
+    const id = useParams();
+    const ind = data.find((item) => item.id === Number(id));
+
     return (
         <main className='sidebar-container'>
             <nav className='sidebar-content'>
-                <Link className='sidebar-link' href={`/sector`}>Promoções</Link>
-                <Link className='sidebar-link' href={`/sector`}>Lançamentos</Link>
-                <Link className='sidebar-link' href={`/sector`}>Vestidos</Link>
-                <Link className='sidebar-link' href={`/sector`}>Acessórios</Link>
+                <Link className='sidebar-link' to={`/sector/promotions`}>Promoções</Link>
+                <Link className='sidebar-link' to={`/sector/releases`}>Lançamentos</Link>
+                <Link className='sidebar-link' to={`/sector/dresses`}>Vestidos</Link>
+                <Link className='sidebar-link' to={`/sector/accessories`}>Acessórios</Link>
                 <Search />
             </nav>
         </main>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -18,7 +18,7 @@ export default function Router() {
                 <Route Component={Product} path="/product/:index" />
                 <Route Component={Cart} path="/cart" />
                 <Route Component={Contact} path="/contact" />
-                <Route Component={Sectors} path="/sector"/>
+                <Route path="/sector/:category" element={<Sectors />} />
                 <Route Component={Err} path="*" />
             </Routes>
         </BrowserRouter>
