888b0576dd61447ece9728c9444e7e28e4659151
PauloBezz <paulo.bezerra2057@gmail.com>
2025-03-02 00:14:06 -0300

    feat: route validation for dynamic URL

--- src/pages/Sectors/index.jsx
+++ src/pages/Sectors/index.jsx
@@ -1,14 +1,69 @@
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { AllSector } from "../../components/AllSector";
 import Header from "../../components/Header";
-import { AllSector } from "../../components/Sector";
 import Sidebar from "../../components/Sidebar";
-import data from '../../data/info.json'
+import data from '../../data/info.json';
 
 export default function Sectors() {
+    const { category } = useParams(); // Para receber a categoria da URL
+    const navigate = useNavigate(); // Redireciona para página correta
+
+    // Categorias válidas
+    const validCategories = ["promotions", "releases", "dresses", "accessories"];
+
+    // Verificação de categoria válida
+    if (!validCategories.includes(category)) {
+        navigate("/");
+        return null;
+    }
+
+    //Filtra os dados
+    const filterCategory = data.sectors[category] || [];
+
     return (
         <>
             <Header />
-            <Sidebar />
-            <AllSector data={data.sectors} />
+            <Sidebar data={filterCategory} />
+            <AllSector data={filterCategory} />
         </>
-    )
+    );
 }
+
+
+
+
+
+
+// import { useParams } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import { AllSector } from "../../components/Sector";
+// import Header from "../../components/Header";
+// import Sidebar from "../../components/Sidebar";
+// import sectors from '../../data/info.json'
+
+
+// export default function Sectors() {
+//     const { category } = useParams(); //Para receber a categoria da URL
+//     const navigate = useNavigate(); // Redireciona para página correta
+
+//     //Categorias válidas
+//     const validCategories = ["promotions", "releases", "dresses", "accessories"];
+
+//     // Verificação
+//     if (!validCategories.includes(category)) {
+//         navigate("/");
+//         return null;
+//     }
+
+//     const filterCategory = sectors[category]
+//     const fil = sectors[category].includes
+
+//     return (
+//         <>
+//             <Header />
+//             <Sidebar data={filterCategory} />
+//             <AllSector data={filterCategory}/>
+//         </>
+//     )
+// }
