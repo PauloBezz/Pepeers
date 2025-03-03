4988f1abc58cc057718c7882b74f81a58875cf18
PauloBezz <paulo.bezerra2057@gmail.com>
2025-03-02 23:56:04 -0300

    refactor: Product component

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,14 +1,12 @@
 import Header from "../../components/Header";
 import Selection from "../../components/Selection";
 import { clothing } from '../../data/info.json'
-import {Cart} from "../../components/Cart/index"
 
 export const Product = () => {
     return (
         <>
             <Header />
             <Selection data={clothing} />
-            {/* <Cart/> */}
         </>
     )
 }
