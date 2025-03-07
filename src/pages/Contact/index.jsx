dad37dedd46e7e4d95120c2557be0c74c2f4016b
PauloBezz <paulo.bezerra2057@gmail.com>
2025-03-07 00:31:52 -0300

    feat: add Footer component in Contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,14 +1,15 @@
 import About from "../../components/About"
 import Header from "../../components/Header"
-
+import Footer from "../../components/Footer"
 
 export const Contact = () => {
 
-    
+
     return (
         <>
-            <Header/>
-            <About/>
+            <Header />
+            <About />
+            <Footer />
         </>
     )
 }
