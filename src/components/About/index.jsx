b652e29573ef8b4da14ce93d00491ac217e05f0b
PauloBezz <paulo.bezerra2057@gmail.com>
2025-03-06 23:59:03 -0300

    feat: add jpg

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,15 +1,14 @@
-
-
+import closet from "./assets/closet.jpg"
 
 export default function About() {
     return (
         <section className="container-about">
             <div className="about-image">
-                <img src="" alt="" />
+                <img src={closet} alt="" />
             </div>
             <span>
                 <h1>Nossa História</h1>
-                <p>Desde 2020, estamos criando tendências espetaculares e trabalhando na evolução da marca.</p>
+                <p>Desde 2020, anunciamos as peças mais elegantes para nossas clientes, independendente de gosto a sensação de vestir e olhar no espelho o resultado é impactante, criar tendências, moldar com suas medidas é um fato de esperar pelo melhor, porque boas coisas podem estar apenas cliques de distância. Faça parte do sonho Pepeers, trabalhamos cada vez mais na evolução da marca.</p>
             </span>
         </section>
     )
