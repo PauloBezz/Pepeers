63a45418136aa351e34295c748c846569361721f
PauloBezz <paulo.bezerra2057@gmail.com>
2025-03-08 21:55:45 -0300

    fix: structure and style About component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,21 @@
 import "./styles.css"
-import closet from "./assets/closet.jpg"
 
 export default function About() {
     return (
         <section className="about-container">
-            <span className="about-content">
-                <h1>Nossa História</h1>
-                <p>Desde 2020, anunciamos elegância para nossas clientes, independendente de gosto a sensação de vestir e olhar no espelho é euforia pura, criar tendências, moldar com suas medidas é um fato de esperar pelo melhor, porque boas coisas podem estar apenas cliques de distância. Faça parte do sonho Pepeers, trabalhamos cada vez mais na evolução da marca.</p>
-            </span>
+            <div className="about-content">
+                <h2>Nossa História</h2>
+                <h5>Desde 2018, anunciamos elegância para nossas clientes, criando tendências, moldando com suas medidas e tornou realidade o fato de esperar pelo nosso melhor, porque boas coisas podem estar apenas cliques de distância.</h5>
+
+                <ul className="about-list">
+                    <li>2018 - Início de um sonho ...</li>
+                    <li>2019 - Primeiras 800 vendas</li>
+                    <li>2020 - Abertura da terceira loja</li>
+                    <li>2023 - Vendas online</li>
+                    <li>2025 - Deu tudo certo.</li>
+                </ul>
+
+            </div>
         </section>
     )
 }
